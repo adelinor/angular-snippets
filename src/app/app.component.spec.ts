7d419c2056defc226fa5181dad4f51e5419c340d
adelinor <adelinor@gmail.com>
2017-12-07 21:18:57 +0000

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { FactorialService } from './rxjs-recursive-observable/factorial/factorial.service';
+import { RouterOutletStubComponent } from '../testing/router-stubs';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        RouterOutletStubComponent
       ],
       providers: [
         FactorialService
---
+++ src/testing/router-stubs.ts
@@ -0,0 +1,20 @@
+import { Component, Directive, Input } from "@angular/core";
+
+@Component({selector: 'router-outlet', template: ''})
+export class RouterOutletStubComponent { }
+
+@Directive({
+    selector: '[routerLink]',
+    host: {
+      '(click)': 'onClick()'
+    }
+  })
+export class RouterLinkStubDirective {
+
+    @Input('routerLink') linkParams: any;
+    navigatedTo: any = null;
+
+    onClick() {
+        this.navigatedTo = this.linkParams;
+    }
+}
