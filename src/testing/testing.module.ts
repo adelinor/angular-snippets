52e2b1a1dfeacc63148b42a0ab5e7e65264b84c4
adelinor <adelinor@gmail.com>
2017-12-07 22:24:28 +0000

    Fix build prod

---
+++ src/testing/testing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterOutletStubComponent, RouterLinkStubDirective } from './router-stubs';
+
+
+@NgModule({
+  declarations: [
+    RouterOutletStubComponent,
+    RouterLinkStubDirective
+  ]
+})
+export class TestingModule { }
