3ef5158d2f4f33770e7a4d0a4fe145af8a53096e
adelinor <adelinor@gmail.com>
2018-05-13 22:22:16 +0100

    Investigating https://github.com/angular/angular/issues/14707

--- src/app/mocking/mocking.module.ts
+++ src/app/mocking/mocking.module.ts
@@ -23,7 +23,7 @@ export class MockingModule {
   }
 
   static forRoot(): ModuleWithProviders {
-      return {
+    return {
         ngModule: MockingModule,
         providers: [
           SampleService
